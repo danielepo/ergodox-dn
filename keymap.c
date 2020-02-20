c30165871853ea1580cfb3acf3e6cc0acaea9287
Daniele Pozzobon <daniele.pozzobon@modine.com>
2020-02-20 12:37:58 +0100

    changed code

--- keymap.c
+++ keymap.c
@@ -5,7 +5,11 @@
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
-#define MDIA 2 // media keys
+#define MOVE 2 // media keys
+#define MDIA 3 // media keys
+
+#define _T_        KC_TRANSPARENT
+#define ___        KC_NO
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -18,43 +22,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  | Grv  |           | CAPS |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | ESC    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ * |Grv/Ctrl|  Win |  Alt |  L2  |  L1  |                                       |  L3  |  L4  |   [  |   ]  | AltGrd |
+ * `------------------------------------'                                       `--------------------------------------'
+ *                                        ,-------------.       ,--------------.
+ *                                        | App  |      |       | LGui |  Esc  |
+ *                                 ,------|------|------|       |------+-------+------.
+ *                                 |      |      | Home |       | PgUp |       |      |
+ *                                 | Space|Backsp|------|       |------|  Tab  |Enter |
+ *                                 |      |ace   | End  |       | PgDn |       |      |
+ *                                 `--------------------'       `---------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+        KC_EQUAL,      KC_1,   KC_2,   KC_3,    KC_4,   KC_5, KC_GRV,
+        KC_TAB ,       KC_Q,   KC_W,   KC_E,    KC_R,   KC_T, ___,
+        MO(MOVE),        KC_A,   KC_S,   KC_D,    KC_F,   KC_G,
+        KC_LSFT,       KC_Z,   KC_X,   KC_C,    KC_V,   KC_B, ___,
+        CTL_T(KC_GRV), KC_LWIN,KC_LALT,MO(SYMB),MO(MOVE),
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
-        // right hand
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_LALT,        CTL_T(KC_ESC),
+                                              KC_SPC ,KC_BSPC,KC_END,
+        // right handf888
+             KC_CAPS, KC_6,     KC_7,    KC_8,    KC_9,   KC_0,          KC_MINS,
+             ___,     KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,          KC_BSLS,
+                      KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN,       KC_QUOT,
+             ___,     KC_N,     KC_M,    KC_COMM, KC_DOT, CTL_T(KC_SLSH),KC_RSFT,
+                                MO(MDIA),MO(MOVE),KC_LBRC,KC_RBRC,       KC_FN1,
+
+             KC_LALT, KC_ESC,
              KC_PGUP,
              KC_PGDN,KC_TAB, KC_ENT
     ),
@@ -82,25 +87,65 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       VRSN,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-          EPRM,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       RGB_MOD,KC_TRNS,
-                                               KC_TRNS,
-                               RGB_VAD,RGB_VAI,KC_TRNS,
+       KC_1,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  _T_,
+       _T_,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,_T_,
+       _T_,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       _T_,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,_T_,
+          EPRM,_T_,_T_,_T_,_T_,
+                                       RGB_MOD,_T_,
+                                               _T_,
+                               RGB_VAD,RGB_VAI,_T_,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+       _T_, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       _T_, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
+                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, _T_,
+       _T_, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, _T_,
+                         _T_,KC_DOT,  KC_0,    KC_EQL,  _T_,
        RGB_TOG, RGB_SLD,
-       KC_TRNS,
-       KC_TRNS, RGB_HUD, RGB_HUI
+       _T_,
+       _T_, RGB_HUD, RGB_HUI
+),/* Keymap 2: Media and mouse keys
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        | Esc  |      |      |      |      |      |           |      | PgUp | Home |  Up  | Fine |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        | Alt  |      | Shit | Ctrl |      |------|           |------| PgDwn| Left | Down |Right |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        | Undo | Cut  | Copy |Paste |      |      |           |      |      |      |      |      |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 | Enter|      |------|       |------|      | Enter|
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// Movement
+[MOVE] = LAYOUT_ergodox(
+       KC_2, ___,     ___,   ___,    ___,     ___, ___,
+       ___, KC_ESC , ___,   ___,    ___,     ___, ___,
+       ___, KC_LALT, ___,   KC_LSFT,KC_LCTL, ___,
+       ___, KC_UNDO, KC_CUT,KC_COPY,KC_PASTE,___, ___,
+       ___, ___,     ___,   _T_,    _T_,
+                                           ___, ___,
+                                                ___,
+                                  KC_ENT , ___, ___,
+    // right hand
+       ___,  ___,    ___,    ___,     ___,     ___, ___,
+       ___,  KC_PGUP,KC_HOME,KC_UP  , KC_END ,KC_DEL , ___,
+             KC_PGDN,KC_LEFT,KC_DOWN, KC_RGHT,KC_BSPC, ___,
+       ___,  ___,    ___,    ___,     ___,     ___, ___,
+                     ___,    ___,     ___,     ___, ___,
+       ___, ___,
+       ___,
+       ___, ___, KC_ENT
 ),
-/* Keymap 2: Media and mouse keys
+/* Keymap 3: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
@@ -123,7 +168,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 [MDIA] = LAYOUT_ergodox(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
--- readme.md
+++ readme.md
@@ -1,41 +1,15 @@
-# ErgoDox EZ
+# ErgoDox EZ Default Configuration
 
-The Ez uses the [Teensy Loader](https://www.pjrc.com/teensy/loader.html).
+## Changelog
 
-Linux users need to modify udev rules as described on the [Teensy
-Linux page].  Some distributions provide a binary, maybe called
-`teensy-loader-cli`.
+* Dec 2016:
+  * Added LED keys
+  * Refreshed layout graphic, comes from http://configure.ergodox-ez.com now.
+* Sep 22, 2016:
+  * Created a new key in layer 1 (bottom-corner key) that resets the EEPROM.
+* Feb 2, 2016 (V1.1): 
+  * Made the right-hand quote key double as Cmd/Win on hold. So you get ' when you tap it, " when you tap it with Shift, and Cmd or Win when you hold it. You can then use it as a modifier, or just press and hold it for a moment (and then let go) to send a single Cmd or Win keystroke (handy for opening the Start menu on Windows).
 
-[Teensy Linux page]: https://www.pjrc.com/teensy/loader_linux.html
+This is what we ship with out of the factory. :) The image says it all:
 
-To flash the firmware:
-
-  - Build the firmware with `make <keyboardname>:<keymapname>`, for example `make ergodox_ez:default`
-
-  - This will result in a hex file called `ergodox_ez_keymapname.hex`, e.g.
-    `ergodox_ez_default.hex`
-
-  - Start the teensy loader.
-
-  - Load the .hex file into it.
-
-  - Press the Reset button by inserting a paperclip gently into the reset hole
-    in the top right corner.
-
-  - Click the button in the Teensy app to download the firmware.
-
-See also [video demonstration](https://www.youtube.com/watch?v=9PyiGUO9_KQ) using Teensy in auto mode.
-
-To flash with ´teensy-loader-cli´:
-
-  - Build the firmware with `make keymapname`, for example `make default`
-
-  - Run ´<path/to/>teensy_loader_cli -mmcu=atmega32u4 -w ergodox_ez_<keymap>.hex´
-
-  - Press the Reset button by inserting a paperclip gently into the reset hole
-    in the top right corder.
-
-## Settings
-
-You may want to enable QMK_KEYS_PER_SCAN because the Ergodox has a relatively
-slow scan rate.
+![Default](https://i.imgur.com/Be53jH7.png)
